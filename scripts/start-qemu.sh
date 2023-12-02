513b1c8642d0bd5c31476fd7338d3188bdc5b6b8
Aleksander Bobinski <aleksander.bobinski@interia.eu>
2023-12-02 11:27:44 +0100

    scripts/start-qemu.sh: Start QEMU with 4 cores
    This makes bootup slightly more bearable.

--- scripts/start-qemu.sh
+++ scripts/start-qemu.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 
 qemu-img create /tmp/test.img 128G
-qemu-system-x86_64 --cdrom "$(realpath archlinux-2023.11.01-x86_64.iso)" --hda /tmp/test.img -m 4096
+qemu-system-x86_64 \
+	--cdrom "$(realpath archlinux-2023.11.01-x86_64.iso)" \
+	--hda /tmp/test.img \
+	-m 4096 \
+	-smp sockets=1,cores=4 \
