f7e1eb531de5b856e786b6bfa5251d4f7b038f6b
Aleksander Bobinski <aleksander.bobinski@interia.eu>
2024-03-11 22:55:09 +0100

    main.tf: Store tfstate in daneborg S3
    The S3 bucket is not going away anytime soon. Store both the state for
    it and the data there.

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "daneborg"
+    key    = "daneborg.tfstate"
+    region = "eu-central-1"
+  }
 }
 
 provider "aws" {
