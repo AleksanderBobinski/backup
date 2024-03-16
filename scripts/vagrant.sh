591b4b2d0768fac2019683d97e8db3e574c812dc
Aleksander Bobinski <aleksander.bobinski@interia.eu>
2024-03-16 16:22:58 +0100

    Add scripts/vagrant.sh
    This script install yay, aconfmgr and attempts to apply the desired
    configuration in a latest archlinux vagrant box.

---
+++ scripts/vagrant.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+set -x
+set -e
+
+vagrant box update
+vagrant up
+vagrant ssh -c 'sudo pacman-key --refresh'
+vagrant ssh -c 'sudo pacman -Syyu --noconfirm --needed git base-devel'
+vagrant ssh -c 'rm -Rf ~/yay-bin'
+vagrant ssh -c 'git clone https://aur.archlinux.org/yay-bin.git ~/yay-bin'
+vagrant ssh -c 'cd ~/yay-bin && makepkg -si --noconfirm --needed'
+vagrant ssh -c 'yay -S --noconfirm aconfmgr-git'
+vagrant ssh -c 'rm -Rf ~/backup'
+vagrant ssh -c 'git clone https://github.com/AleksanderBobinski/backup.git ~/backup'
+vagrant ssh -c 'cd ~/backup/aconfmgr &&  aconfmgr --aur-helper yay --config ./ apply'
