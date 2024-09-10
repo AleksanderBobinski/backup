5550ffb5755b7de7ffa087f98796fcf9bfb3e7ae
Aleksander Bobinski <aleksander.bobinski@interia.eu>
2024-09-10 23:55:19 +0200

    aconfmgr: Ignore etc/shells
    This file consists of sane defaults, there is no reason to track changes
    in it.

--- aconfmgr/16-boot.sh
+++ aconfmgr/16-boot.sh
@@ -4,5 +4,4 @@ CopyFile /etc/mkinitcpio.d/linux.preset
 CopyFile /etc/pam.d/sddm
 CopyFile /etc/sddm.conf.d/kde_settings.conf
 CreateFile /etc/sddm.conf > /dev/null
-CopyFile /etc/shells
 CopyFile /etc/mkinitcpio.d/linux.preset
