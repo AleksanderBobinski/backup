d13fce219e3f6e009b40b603e913591d7f36d051
Aleksander Bobinski <aleksander.bobinski@interia.eu>
2024-12-14 21:57:35 +0100

    scripts/borg.sh: Backup printed out photos as well

--- scripts/borg.sh
+++ scripts/borg.sh
@@ -3,4 +3,4 @@ set -e
 set -x
 
 borg init --progress --append-only --storage-quota 100G --encryption=repokey ./borg || true
-borg create --progress --compression lzma ./borg::'dane-{now:%Y%m%d%H%M%S}' /Dane/finanse/ /Dane/hobby /Dane/notatki /Dane/pamiątki /Dane/praca /Dane/studia /Dane/zdjęcia/do_wydrukowania/
+borg create --progress --compression lzma ./borg::'dane-{now:%Y%m%d%H%M%S}' /Dane/finanse/ /Dane/hobby /Dane/notatki /Dane/pamiątki /Dane/praca /Dane/studia /Dane/zdjęcia/do_wydrukowania/ /Dane/zdjęcia/wydrukowane/
