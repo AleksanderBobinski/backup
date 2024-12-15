2159436b720a0ad162c5e433a60236419bf0d25e
Aleksander Bobinski <aleksander.bobinski@interia.eu>
2024-12-15 08:58:59 +0100

    aconfmgr: Use default /etc/hosts
    The current one I use apparently has machines which I used 7 years ago.

--- aconfmgr/18-network.sh
+++ aconfmgr/18-network.sh
@@ -3,4 +3,3 @@ CreateDir /etc/cni/net.d 700
 CopyFile /etc/NetworkManager/conf.d/dns.conf
 CreateDir /etc/openvpn/client 750 openvpn network
 CreateDir /etc/openvpn/server 750 openvpn network
-CopyFile /etc/hosts
