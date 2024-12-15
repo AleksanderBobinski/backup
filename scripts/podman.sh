be7b64b0d55929f8b14fb9ce52237ab42eb0e877
Aleksander Bobinski <aleksander.bobinski@interia.eu>
2024-12-15 20:15:09 +0100

    scripts/podman.sh: sudo requires a tty
    Even if sudo is configured to not ask for a password it will still
    complain about a missing tty:
      sudo: a terminal is required to read the password; either use the -S
      option to read from standard input or configure an askpass helper
      sudo: a password is required

--- scripts/podman.sh
+++ scripts/podman.sh
@@ -3,6 +3,6 @@ set -x
 set -e
 
 podman build --tag testme .
-podman run --rm --name testme --replace --interactive --volume "$(pwd)":"/tmp/backup/":ro testme <<EOF
+podman run --rm --name testme --replace --interactive --tty --volume "$(pwd)":"/tmp/backup/":ro testme <<EOF
 cd "/tmp/backup/aconfmgr" &&  aconfmgr --aur-helper yay --config ./ apply --yes --verbose
 EOF
