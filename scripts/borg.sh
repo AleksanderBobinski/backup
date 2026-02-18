b581eb7d7efc1cb5287a43a4961171fecfde594a
Aleksander Bobinski <aleksander.bobinski@interia.eu>
2026-02-18 07:42:26 +0100

    scripts/borg.sh: Keep all photos

--- scripts/borg.sh
+++ scripts/borg.sh
@@ -3,5 +3,5 @@ set -e
 set -x
 
 borg init --progress --storage-quota 100G --encryption=repokey ./borg || true
-borg create --progress --compression lzma ./borg::'dane-{now:%Y%m%d%H%M%S}' /Dane/finanse/ /Dane/hobby /Dane/notatki /Dane/pamiątki /Dane/praca /Dane/studia /Dane/zdjęcia/do_wydrukowania/ /Dane/zdjęcia/wydrukowane/ /Dane/gwarancje/ /Dane/mieszkania/ /Dane/pamiątki
+borg create --progress --compression lzma ./borg::'dane-{now:%Y%m%d%H%M%S}' /Dane/finanse/ /Dane/hobby /Dane/notatki /Dane/pamiątki /Dane/praca /Dane/studia /Dane/zdjęcia/ /Dane/zdjęcia/ /Dane/gwarancje/ /Dane/mieszkania/ /Dane/pamiątki
 borg create --progress --compression lzma ./borg::'mail-{now:%Y%m%d%H%M%S}' "$HOME/.thunderbird/5fkf29or.default"
