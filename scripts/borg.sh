dc0d5b6ffba4b74a6f98beaab5b71517a8a8302c
Aleksander Bobinski <aleksander.bobinski@interia.eu>
2026-02-20 10:20:19 +0100

    scripts/borg.sh: Read password once

--- scripts/borg.sh
+++ scripts/borg.sh
@@ -1,7 +1,11 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -e
-set -x
 
+echo "Password:"
+read -s -r BORG_PASSPHRASE
+export BORG_PASSPHRASE=$BORG_PASSPHRASE
+
+set -x
 borg init --progress --storage-quota 100G --encryption=repokey ./borg || true
 borg create --progress --compression lzma ./borg::'dane-{now:%Y%m%d%H%M%S}' /Dane/finanse/ /Dane/hobby /Dane/notatki /Dane/pamiątki /Dane/praca /Dane/studia /Dane/zdjęcia/ /Dane/zdjęcia/ /Dane/gwarancje/ /Dane/mieszkania/ /Dane/pamiątki
 borg create --progress --compression lzma ./borg::'mail-{now:%Y%m%d%H%M%S}' "$HOME/.thunderbird/5fkf29or.default"
