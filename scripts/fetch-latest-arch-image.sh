309c543fdd77ecc92fce8b7ce9d5a212743c63ac
Aleksander Bobinski <aleksander.bobinski@interia.eu>
2026-03-02 22:47:09 +0100

    Drop unused scripts

--- scripts/fetch-latest-arch-image.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-set -x
-set -e
-
-wget --continue --tries=0 --timeout 10 mirroronet.pl/pub/mirrors/archlinux/iso/latest/archlinux-x86_64.iso
-wget --continue --tries=0 --timeout 10 mirroronet.pl/pub/mirrors/archlinux/iso/latest/sha256sums.txt
-sha256sum --ignore-missing --check ./sha256sums.txt
-rm sha256sums.txt
--- scripts/podman.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-set -x
-set -e
-
-podman build --tag testme .
-podman run --rm --name testme --replace --interactive --tty --volume "$(pwd)":"/tmp/backup/":ro testme <<EOF
-cd "/tmp/backup/aconfmgr" &&  aconfmgr --aur-helper yay --config ./ apply --yes --verbose
-EOF
