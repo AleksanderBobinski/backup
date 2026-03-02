86600cbc462bc67af00b724369c2b7f99c748aa7
Aleksander Bobinski <aleksander.bobinski@interia.eu>
2026-03-02 23:54:33 +0100

    scripts/borg.sh: Use new source location

--- scripts/borg.sh
+++ scripts/borg.sh
@@ -7,5 +7,5 @@ export BORG_PASSPHRASE=$BORG_PASSPHRASE
 
 set -x
 borg init --progress --storage-quota 100G --encryption=repokey ./borg || true
-borg create --progress --compression lzma ./borg::'dane-{now:%Y%m%d%H%M%S}' /Dane/finanse/ /Dane/hobby /Dane/notatki /Dane/pamiątki /Dane/praca /Dane/studia /Dane/zdjęcia/ /Dane/zdjęcia/ /Dane/gwarancje/ /Dane/mieszkania/ /Dane/pamiątki
+borg create --progress --compression lzma ./borg::'dane-{now:%Y%m%d%H%M%S}' "$HOME/ws/dane"
 borg create --progress --compression lzma ./borg::'mail-{now:%Y%m%d%H%M%S}' "$HOME/.thunderbird/5fkf29or.default"
